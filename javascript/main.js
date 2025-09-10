dfef3129f391fb849a3927e490d19c731a2f35c3
Simon Tech <contato.simon.tech@gmail.com>
2025-09-10 11:16:40 -0300

    main init

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,9 @@
 //lógica de escolhas
+
+//texto base: "FINALIDADE: (CONFORME LEI ESTADUAL N.º 15.266 DE 26 DE DEZEMBRO DE 2013) VALOR EM UFESP: *valor*  (TAXA DE FISCALIZAÇÃO PARA RENOVAÇÃO DA LICENÇA DE FUNCIONAMENTO PARA ATIVIDADE DE FORNECIMENTO DE ALIMENTOS PREPARADOS PREPONDERANTEMENTE PARA EMPRESAS)"
+
+//valores
+// - Indústria de alimentos e sub-itens: R$ 4072,20
+// - Indústria de alimentos, 1.1.1.8.2 Sorveteria R$ 1628,88
+// - Indústria de alimentos, 1.1.1.25 Fabricação de produtos de padaria e confeitaria produção própria R$ 1221,66
+// - Indústria de alimentos, 1.1.1.39 Atividades de armazenamento de alimentos em depósito fechado R$ 1221,66
